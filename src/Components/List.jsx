1d74f30ecbbeade2cd31de6d7e1044fedc525f0d
mustapha jay <mustaphajay2@gmail.com>
2022-05-20 16:01:32 +0100

    initial commit

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,1 +1,9 @@
-rafce;
+import React from 'react'
+
+const List = () => {
+  return (
+    <div>List</div>
+  )
+}
+
+export default List
---
+++ src/Components/Search_form.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Search_form = () => {
+  return (
+    <div>Search_form</div>
+  )
+}
+
+export default Search_form
