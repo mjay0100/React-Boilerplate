186236c4630445a310df10d38969332f37c9dc49
mustapha jay <mustaphajay2@gmail.com>
2022-05-31 14:16:08 +0100

    added cocktails states and loop through also added cocktail component

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { useGlobal } from "../Context";
+import Cocktail from "./Cocktail";
 
 const List = () => {
-  const { loading } = useGlobal();
+  const { loading, cocktails } = useGlobal();
+
   if (loading) {
-    return (
-      <h1 className="text-4xl text-center text-red-500 capitalize">
-        loading...
-      </h1>
-    );
+    return <h1 className="text-4xl text-center  capitalize">loading...</h1>;
   }
-  return <div>List</div>;
+  if (cocktails.length < 1) {
+    return <h1 className="text-4xl text-center  capitalize">No match</h1>;
+  }
+  return (
+    <div className="grid grid-cols-3 gap-[2.5rem] w-[70vw] mx-auto mt-[7rem] rounded-lg">
+      {cocktails.map((drink) => {
+        return <Cocktail key={drink.id} {...drink} />;
+      })}
+    </div>
+  );
 };
 
 export default List;
