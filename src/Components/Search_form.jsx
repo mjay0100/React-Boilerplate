9f89ff9fe7e25a04be63c9bf24648e380bf1bb00
mustapha jay <mustaphajay2@gmail.com>
2022-05-31 14:17:25 +0100

    added logic for the search form

--- src/Components/Search_form.jsx
+++ src/Components/Search_form.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useRef, useEffect } from "react";
+import { useGlobal } from "../Context";
 
 const Search_form = () => {
-  return (
-    <div>Search_form</div>
-  )
-}
+  const { setSearchTerm } = useGlobal();
+  const search = useRef();
+  useEffect(() => {
+    search.current.focus();
+  }, []);
 
-export default Search_form
+  const cocktailsearch = () => {
+    setSearchTerm(search.current.value);
+  };
+  return (
+    <div className="shadow-sm shadow-zinc-400/80 hover:shadow-zinc-700 duration-500 transition-all w-1/2 mt-[5rem] mx-auto p-4 ">
+      <form className="text-center grid " onSubmit={(e) => e.preventDefault()}>
+        <label
+          htmlFor="name"
+          className="text-2xl mt-5 capitalize text-emerald-600"
+        >
+          search cocktails
+        </label>
+        <input
+          className="w-[60%] mx-auto my-4 p-1  border-2 border-solid border-transparent bg-[#f1f5f8] rounded-sm focus:border-red-500"
+          type="text"
+          id="name"
+          onChange={cocktailsearch}
+          ref={search}
+          placeholder="search cocktail"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Search_form;
